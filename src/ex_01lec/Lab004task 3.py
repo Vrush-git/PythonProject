70139ecb18472860c2114287c0781290669729ba
Vrush-git <vrushh31@gmail.com>
2024-12-11 23:10:28 +0530

    new lab wih task

---
+++ src/ex_01lec/Lab004task 3.py
@@ -0,0 +1,10 @@
+#program of add,sum,sub,mul and div
+
+num1=int(input("Enter num1"))
+num2=int(input("Enter num2"))
+print("Maximum no is ", max(num1,num2))
+print("Sum is",num1+num2)
+print("sub of this is", num1-num2)
+print("power of ",pow(num1,num2))
+print("Division is ", (num1/num2))
+print("Anser is",(num1*num2))
